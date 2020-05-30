203e72f4e659eaa2d99c4d0861dad2834f2cdc99
simyeongryu <rsm0503@gmail.com>
2020-05-30 23:58:32 +0900

    ㅊㅗ기화

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Contents from "./Contents";
+
+export default () => (
+  <>
+    <Header />
+    <Contents />
+    <Footer />
+  </>
+);
